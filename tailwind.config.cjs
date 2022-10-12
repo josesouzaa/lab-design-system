034da922fb971e2a1e648fb4e3884134b2be959f
josesouzaa <josee.souzaaa@gmail.com>
2022-10-12 11:58:48 -0300

    Setup de cores no Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
+    colors: {
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6'
+      },
+      cyan: {
+        500: '#81d8f7',
+        300: '#9be1fb'
+      }
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
